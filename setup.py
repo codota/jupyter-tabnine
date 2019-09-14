41b9a6dcba3eac1cd6466b21a30759e3e0cd2fc1
Wenmin Wu <wuwenmin1991@gmail.com>
2019-09-14 21:07:52 +0800

    modify setup

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import setuptools
+from glob import glob
 
 setuptools.setup(
     name="jupyter_tabnine",
@@ -8,13 +9,15 @@ setuptools.setup(
     author_email="wuwenmin1991@gmail.com",
     license="GPL-3.0",
     description="Jupyter notebook extension which support coding auto-completion based on Deep Learning",
-    packages=find_packages('src'),
+    packages=setuptools.find_packages('src'),
     package_dir={'': 'src'},
     install_requires=['ipython', 'jupyter_core', 'nbconvert', 'notebook >=4.2',],
     python_requires='>=3.5',
     classifiers=[
         'Framework :: Jupyter',
     ],
+    data_files=[('share/jupyter/nbextensions/jupyter_tabnine',
+                 glob('src/jupyter_tabnine/static/*'))],
     include_package_data=True,
     zip_safe=False
 )
