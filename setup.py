c77518fd102b9a576601caae3bfdc692acb7dc47
吴文敏 <wuwenmin1991@gmail.com>
2019-09-15 20:35:29 +0800

    Update setup.py
    add description

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 import setuptools
-from glob import glob
+
+with open('./README.rst') as f:
+    readme = f.read()
 
 setuptools.setup(
     name="jupyter_tabnine",
-    version='1.0.0',
+    version='1.0.1',
     url="https://github.com/wenmin-wu/jupyter-tabnine",
     author="Wenmin Wu",
+    long_description=readme,
+    long_description_content_type="text/x-rst",
     author_email="wuwenmin1991@gmail.com",
     license="GPL-3.0",
     description="Jupyter notebook extension which support coding auto-completion based on Deep Learning",
@@ -16,8 +20,6 @@ setuptools.setup(
     classifiers=[
         'Framework :: Jupyter',
     ],
-    data_files=[('share/jupyter/nbextensions/jupyter_tabnine',
-                 glob('src/jupyter_tabnine/static/*'))],
     include_package_data=True,
     zip_safe=False
 )
