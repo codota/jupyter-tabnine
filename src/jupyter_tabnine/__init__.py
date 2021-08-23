453738a663df69eddae3e218e66a4a138fbe5293
Amir Bilu <amirbilu@codota.com>
2021-08-23 14:27:57 +0300

    fix backslash path

--- src/jupyter_tabnine/__init__.py
+++ src/jupyter_tabnine/__init__.py
@@ -1,20 +1,22 @@
 from notebook.utils import url_path_join as ujoin
-from .handler import TabNineHandler
-from .tabnine import TabNine
+from .handler import TabnineHandler
+from .tabnine import Tabnine
 
 # Jupyter Extension points
 def _jupyter_server_extension_paths():
-    return [{
-        'module': 'jupyter_tabnine',
-    }]
+    return [{"module": "jupyter_tabnine",}]
+
 
 def _jupyter_nbextension_paths():
-    return [{
-        "section": "notebook",
-        "dest": "jupyter_tabnine",
-        'src': 'static',
-        "require": "jupyter_tabnine/main"
-    }]
+    return [
+        {
+            "section": "notebook",
+            "dest": "jupyter_tabnine",
+            "src": "static",
+            "require": "jupyter_tabnine/main",
+        }
+    ]
+
 
 def load_jupyter_server_extension(nb_server_app):
     """
@@ -24,7 +26,9 @@ def load_jupyter_server_extension(nb_server_app):
         nb_server_app (NotebookWebApplication): handle to the Notebook webserver instance.
     """
     web_app = nb_server_app.web_app
-    host_pattern = '.*$'
-    route_pattern = ujoin(web_app.settings['base_url'], '/tabnine')
-    tabnine = TabNine()
-    web_app.add_handlers(host_pattern, [(route_pattern, TabNineHandler, {'tabnine': tabnine})])
+    host_pattern = ".*$"
+    route_pattern = ujoin(web_app.settings["base_url"], "/tabnine")
+    tabnine = Tabnine()
+    web_app.add_handlers(
+        host_pattern, [(route_pattern, TabnineHandler, {"tabnine": tabnine})]
+    )
